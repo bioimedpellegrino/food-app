fdfa0590a009b8361aff41809312d25bc4cefb96
Alessandro Pellegrino <alessandro.pellegrino9494@gmail.com>
2023-10-01 23:02:31 +0200

    .

---
+++ lib/model/DailyDietModel.dart
@@ -0,0 +1,62 @@
+import 'dart:convert';
+import 'package:flutter_diet_tips/model/PortionModel.dart';
+
+List<DailyDietModel> dailyDietFromJson(String str) =>
+    List<DailyDietModel>.from(json.decode(str).map((x) => DailyDietModel.fromJson(x)));
+
+class DailyDietModel{
+  
+  DailyDietModel({
+    this.name,
+    this.category,
+    this.total_kcal,
+    this.total_carbohydrates,
+    this.total_proteins,
+    this.total_fats,
+    this.portions
+  });
+
+  String? name, category;
+  double? total_kcal, total_carbohydrates, total_proteins, total_fats;
+  List<PortionModel>? portions;
+
+
+  factory DailyDietModel.fromJson(Map<String, dynamic> json) {
+    List<dynamic>? jsonPortions = json['portions'];
+    List<PortionModel>? portions;
+
+    if (jsonPortions != null) {
+      portions = jsonPortions
+          .map((portion) => PortionModel.fromJson(portion))
+          .toList();
+    }
+
+    return DailyDietModel(
+      name: json["name"],
+      category: json["category"],
+      total_kcal: json["total_kcal"]?.toDouble(),
+      total_carbohydrates: json["total_carbohydrates"]?.toDouble(),
+      total_proteins: json["total_proteins"]?.toDouble(),
+      total_fats: json["total_fats"]?.toDouble(),
+      portions: portions,
+    );
+  }
+  
+  Map<String, dynamic> toJson() {
+    List<Map<String, dynamic>>? portionsJson;
+
+    if (portions != null) {
+      portionsJson = portions!.map((portion) => portion.toJson()).toList();
+    }
+
+    return {
+      "name": name,
+      "category": category,
+      "total_kcal": total_kcal,
+      "total_carbohydrates": total_carbohydrates,
+      "total_proteins": total_proteins,
+      "total_fats": total_fats,
+      "portions": portionsJson,
+    };
+  }
+}
---
+++ lib/model/PortionModel.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+
+List<PortionModel> portionModelFromJson(String str) =>
+    List<PortionModel>.from(json.decode(str).map((x) => PortionModel.fromJson(x)));
+class PortionModel{
+  
+  PortionModel({
+    this.id,
+    this.name,
+    this.unity,
+    this.quantity
+  });
+
+  int? id;
+  String? name, unity;
+  double? quantity;
+
+  factory PortionModel.fromJson(Map<String, dynamic> json) => PortionModel(
+        id: json["id"],
+        name: json["name"],
+        unity: json["unity"],
+        quantity: json["quantity"]
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "name": name,
+        "unity": unity,
+        "quantity": quantity
+      };
+}
